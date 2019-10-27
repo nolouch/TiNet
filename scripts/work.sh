b563ddd97103d67ab30f353f9cd201dd461946b8
Notdeath <364763433@qq.com>
2019-10-27 12:22:48 +0800

    add dataset
    Signed-off-by: Notdeath <364763433@qq.com>

--- scripts/work.sh
+++ scripts/work.sh
@@ -1,24 +1,25 @@
 # /bin/bash
-b=40
+# 周期 40 分钟
+cycle=40
 while true 
 do
 	sec=`date +%M`
 	hours=`date +%H`
-	echo $hours:$sec
+	echo $hours:$sec 
 	fg=`expr $hours % 2`
 	hours=`expr $fg \* 60`
 	time=`expr $sec + $hours`
-	time=`expr $time % $b`
+	time=`expr $time % $cycle`
 	if [[ $time = 11 ]];
 	then
 		./start_scale.sh
-		echo "start"
+		echo "start reject-region-scheduler"
 	fi
 	
 	if [[ $time = 31 ]];
-	then 
-		echo "stop"
+	then
 		./stop_scale.sh
+		echo "stop reject-region-scheduler"
 	fi
 	sleep 20
 done
